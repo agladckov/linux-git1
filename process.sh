2998048c4fd207b1386661686e9e2a39be88c9cb
Гладков_Андрей <agladckov@edge1.ru-central1.internal>
2022-11-07 21:06:31 +0000

    avg changed

--- process.sh
+++ process.sh
@@ -1,4 +1,4 @@
-awk -F, '{ total += $16 } END { print "RATING_AVG " total / NR }' $1
+awk -F, '{if ($16 != 0) {num++;} total += $16 } END { print "RATING_AVG " total / num }' $1
 awk -F, '{col[tolower($7)]++} END {for (i in col) print "HOTELNUMBER " tolower(i) " " col[i]}' $1 | sort
 awk -F, '{col[tolower($7)]++; if ($1 ~ /holiday_inn/) {hic[tolower($7)] += $12; hin[tolower($7)]++;} else if ($1 ~ /hilton/){hc[tolower($7)] += $12; hn[tolower($7)]++;} } END {for (i in col) print "CLEANLINESS " i " " hic[i] / hin[i] " " hc[i] / hn[i]}' $1
 gnuplot -persist <<-EOFMarker
@@ -7,5 +7,5 @@ set output 'graph.png'
 set title "Correlation between Cleanliness and overall rating" font ",14" textcolor rgbcolor "royalblue"
 set pointsize 1
 set datafile separator ','
-plot "hotels.csv" using 16, 10 with linespoints
+plot $1 using 16, 10 with linespoints
 EOFMarker
