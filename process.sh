80ae55565250a7150b85c95eda015c8fdd62a2f0
Гладков_Андрей <agladckov@edge1.ru-central1.internal>
2022-11-08 12:03:30 +0000

    fixed avg

--- process.sh
+++ process.sh
@@ -1,10 +1,10 @@
-awk -F, '{if ($16 >= 1.0) {num++; total += $16;} } END { print "RATING_AVG " 0.1 + total / num }' $1
+awk -F, '{if ($18 >= 1.0) {num++; total += $18;} } END { print "RATING_AVG " total / num }' $1
 awk -F, '{col[tolower($7)]++} END {for (i in col) print "HOTELNUMBER " tolower(i) " " col[i]}' $1 | sort
 awk -F, '{col[tolower($7)]++; if ($2 ~ /holiday inn/) {hic[tolower($7)] += $12; hin[tolower($7)]++;} else if ($1 ~ /hilton/){hc[tolower($7)] += $12; hn[tolower($7)]++;} } END {for (i in col) print "CLEANLINESS " i " " hic[i] / hin[i] " " hc[i] / hn[i]}' $1
 gnuplot -persist <<-EOFMarker
 set terminal png size 400,300 enhanced font "Helvetica,20"
 set output 'graph.png'
-set title "Correlation between Cleanliness and overall rating" font ",14" textcolor rgbcolor "royalblue"
+set title "Correlation between Cleanliness and overall rating" font ",12" textcolor rgbcolor "royalblue"
 set pointsize 1
 set datafile separator ','
 plot $1 using 16, 10 with linespoints
