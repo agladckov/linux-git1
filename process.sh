e9049fb37b4988fd4ec173b3a31a4c07717e7fc3
Гладков_Андрей <agladckov@edge1.ru-central1.internal>
2022-11-08 14:58:02 +0000

    fixed tmp

--- process.sh
+++ process.sh
@@ -3,6 +3,7 @@ awk -F, '{col[tolower($7)]++} END {for (i in col) print "HOTELNUMBER " tolower(i
 awk -F, '{col[tolower($7)]++; if ($2 ~ /holiday inn/ && $12 > 0) {hic[tolower($7)] += $12; hin[tolower($7)]++;} else if ($1 ~ /hilton/ && $12 > 0){hc[tolower($7)] += $12; hn[tolower($7)]++;} } END {for (i in col) print "CLEANLINESS " i " " hic[i] / hin[i] " " hc[i] / hn[i]}' $1
 awk -F, '{if ($12 > 0 && $18 > 0) {print $12 ","$18}}' $1 > "./tmp/fit.csv"
 gnuplot -persist <<-EOFMarker
+mkdir -p tmp
 set terminal png size 500,500 enhanced font "Helvetica,14"
 set output 'graph.png'
 set title "Correlation between Cleanliness and overall rating" font ",12" textcolor rgbcolor "royalblue"
