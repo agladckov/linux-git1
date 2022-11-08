bf5cd220cf8f20040913407231f87841e831872b
Гладков_Андрей <agladckov@edge1.ru-central1.internal>
2022-11-08 12:09:50 +0000

    fixed index 12->14

--- process.sh
+++ process.sh
@@ -1,6 +1,6 @@
 awk -F, '{if ($18 >= 1.0) {num++; total += $18;} } END { print "RATING_AVG " total / num }' $1
 awk -F, '{col[tolower($7)]++} END {for (i in col) print "HOTELNUMBER " tolower(i) " " col[i]}' $1 | sort
-awk -F, '{col[tolower($7)]++; if ($2 ~ /holiday inn/) {hic[tolower($7)] += $12; hin[tolower($7)]++;} else if ($1 ~ /hilton/){hc[tolower($7)] += $12; hn[tolower($7)]++;} } END {for (i in col) print "CLEANLINESS " i " " hic[i] / hin[i] " " hc[i] / hn[i]}' $1
+awk -F, '{col[tolower($7)]++; if ($2 ~ /holiday inn/) {hic[tolower($7)] += $14; hin[tolower($7)]++;} else if ($1 ~ /hilton/){hc[tolower($7)] += $14; hn[tolower($7)]++;} } END {for (i in col) print "CLEANLINESS " i " " hic[i] / hin[i] " " hc[i] / hn[i]}' $1
 gnuplot -persist <<-EOFMarker
 set terminal png size 400,300 enhanced font "Helvetica,20"
 set output 'graph.png'
