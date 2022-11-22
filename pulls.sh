76e68819d4439b9f6bfbde936274b8fa22c1998a
Гладков_Андрей <agladckov@edge1.ru-central1.internal>
2022-11-22 19:48:15 +0000

    slash fixed

--- pulls.sh
+++ pulls.sh
@@ -2,46 +2,46 @@
 token="$(<./github_token)"
 curl -H "Accept: application/vnd.github+json" \
 -H "Authorization: Bearer ${token}" \
-"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=0" -o \tmp\info0.json
+"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=0" -o /tmp/info0.json
 
 token="$(<./github_token)"
 curl -H "Accept: application/vnd.github+json" \
 -H "Authorization: Bearer ${token}" \
-"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=1" -o \tmp\info1.json
+"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=1" -o /tmp/info1.json
 
 token="$(<./github_token)"
 curl -H "Accept: application/vnd.github+json" \
 -H "Authorization: Bearer ${token}" \
-"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=2" -o \tmp\info2.json
+"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=2" -o /tmp/info2.json
 
 token="$(<./github_token)"
 curl -H "Accept: application/vnd.github+json" \
 -H "Authorization: Bearer ${token}" \
-"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=3" -o \tmp\info3.json
+"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=3" -o /tmp/info3.json
 
 token="$(<./github_token)"
 curl -H "Accept: application/vnd.github+json" \
 -H "Authorization: Bearer ${token}" \
-"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=4" -o \tmp\info4.json
+"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=4" -o /tmp/info4.json
 
 token="$(<./github_token)"
 curl -H "Accept: application/vnd.github+json" \
 -H "Authorization: Bearer ${token}" \
-"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page5" -o \tmp\info5.json
+"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page5" -o /tmp/info5.json
 
 token="$(<./github_token)"
 curl -H "Accept: application/vnd.github+json" \
 -H "Authorization: Bearer ${token}" \
-"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=6" -o \tmp\info6.json
+"https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=6" -o /tmp/info6.json
 
-cat \tmp\info0.json \tmp\info1.json \tmp\info2.json \tmp\info3.json \tmp\info4.json \tmp\info5.json \tmp\info6.json > \tmp\info.json
+cat /tmp/info0.json /tmp/info1.json /tmp/info2.json /tmp/info3.json /tmp/info4.json /tmp/info5.json /tmp/info6.json > /tmp/info.json
 name="$1"
-num=$(jq --arg NAME "$name" '.[] | select(.user.login==$NAME) | .number' \tmp\info.json | wc -l)
+num=$(jq --arg NAME "$name" '.[] | select(.user.login==$NAME) | .number' /tmp/info.json | wc -l)
 echo "PULLS ${num}"
-jq --arg NAME "$name" '.[] | select(.user.login==$NAME) | .number' \tmp\info.json > \tmp\num
-earliest=$(sort \tmp\num | head -1)
+jq --arg NAME "$name" '.[] | select(.user.login==$NAME) | .number' /tmp/info.json > /tmp/num
+earliest=$(sort /tmp/num | head -1)
 echo "EARLIEST ${earliest}"
-ismerged=$(jq --arg NAME "$name" --arg EARLIEST "$earliest" '.[] | select(.user.login==$NAME) | .merged_at' \tmp\info.json)
+ismerged=$(jq --arg NAME "$name" --arg EARLIEST "$earliest" '.[] | select(.user.login==$NAME) | .merged_at' /tmp/info.json)
 if [ "$ismerged" == "null" ]; then
   ismerged="0"
 else
