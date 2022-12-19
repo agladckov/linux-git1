44e341968fa42e487f347fef9218c7f0efa17c09
Гладков_Андрей <agladckov@edge1.ru-central1.internal>
2022-12-19 01:19:05 +0000

    readme +

--- project2/ansible.sh
+++ project2/ansible.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 ansible all -m ping > ansible.log 2>&1
-
-
+ansible-playbook install.yml >> ansible.log 2>&1
+ansible-playbook info.yml >> ansible.log 2>&1
+cat ./info/*.txt > send.txt
+ansible-playbook --ask-vault-pass send.yml >> ansible.log 2>&1
